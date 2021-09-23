fb9b99e19f0a8441f6677fe10751c3de6142afcb
yuri fardel <yrnascimento@outlook.com>
2021-09-23 00:49:34 -0300

    chore: simple app

---
+++ MinhaThread.java
@@ -0,0 +1,27 @@
+public class MinhaThread extends Thread {
+  public MinhaThread() {
+    start();
+  }
+
+  public void run() {
+
+    try {
+      String animação = "|/-\\";
+      for (int x = 0 ; x <= 100 ; x++) {
+          String data = "\r" + animação.charAt(x % animação.length()) + " " + x + "%";
+          System.out.print(data);
+          Thread.sleep(100);
+      }
+
+    } catch (InterruptedException  e) {
+      e.printStackTrace();
+    }
+
+  }
+}
+
+class teste {
+  public static void main(String[] args) {
+    MinhaThread thr = new MinhaThread();
+  }
+}
